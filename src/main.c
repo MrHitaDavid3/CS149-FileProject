59c21bb0592a2790a1decbfaca7d6f51cc23830e
Johnny Tsai <cljohnnytsai@gmail.com>
2026-05-11 09:35:47 -0700

    Refactor command handling in main.c
    Removed 'close' and 'search' commands from user input options. Updated variable names for clarity and improved input handling.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,37 @@
-int main() {
-    pthread_t t_create, t_open, t_close, t_search;
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "globals.h"
 
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+
+File files[MAX_FILES];
+int file_count = 0;
+
+Command current_command = NONE;
+char current_filename[MAX_FILENAME];
+int current_file_index = -1;
+
+char global_buffer[MAX_CONTENT];
+
+void *create_thread(void *arg);
+void *open_thread(void *arg);
+void *close_thread(void *arg);
+void *search_thread(void *arg);
+
+int main() {
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond, NULL);
+    
+    pthread_t t_create, t_open, t_close, t_search;
 
     pthread_create(&t_create, NULL, create_thread, NULL);
     pthread_create(&t_open, NULL, open_thread, NULL);
     pthread_create(&t_close, NULL, close_thread, NULL);
     pthread_create(&t_search, NULL, search_thread, NULL);
 
-    char cmd[20];
+    char cmd_input[20];
 
     while (1) {
         pthread_mutex_lock(&mutex);
@@ -20,31 +42,24 @@ int main() {
             continue;
         }
 
-        printf("\nCommands: create, open, close, search, exit\n");
+        printf("\nCommands: create, open, exit\n");
         printf("Enter command: ");
-        scanf("%s", cmd);
+        scanf("%19s", cmd_input);
 
-        if (strcmp(cmd, "exit") == 0) {
+        if (strcmp(cmd_input, "exit") == 0) {
             pthread_mutex_unlock(&mutex);
             break;
         }
-
-        if (strcmp(cmd, "close") != 0) {
+        
+        if (strcmp(cmd_input, "create") == 0) {
             printf("Enter filename: ");
-            scanf("%s", current_filename);
-        }
-
-        if (strcmp(cmd, "create") == 0) {
+            scanf("%49s", current_filename);
             current_command = CREATE;
         } 
-        else if (strcmp(cmd, "open") == 0) {
+        else if (strcmp(cmd_input, "open") == 0) {
+            printf("Enter filename: ");
+            scanf("%49s", current_filename);
             current_command = OPEN;
-        } 
-        else if (strcmp(cmd, "close") == 0) {
-            current_command = CLOSE;
-        } 
-        else if (strcmp(cmd, "search") == 0) {
-            current_command = SEARCH;
         } 
         else {
             printf("Invalid command.\n");
@@ -52,14 +67,6 @@ int main() {
 
         pthread_mutex_unlock(&mutex);
     }
-
-    pthread_join(t_create, NULL);
-    pthread_join(t_open, NULL);
-    pthread_join(t_close, NULL);
-    pthread_join(t_search, NULL);
-
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
 
     return 0;
 }
