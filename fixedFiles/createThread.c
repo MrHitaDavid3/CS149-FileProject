5e0c83fd54b8f3ff743202475025189e72f0367b
Johnny Tsai <cljohnnytsai@gmail.com>
2026-05-12 12:24:07 -0700

    Refactor file existence check and improve formatting

--- fixedFiles/createThread.c
+++ fixedFiles/createThread.c
@@ -11,40 +11,41 @@ void *create_thread(void *arg) {
 
         if (current_command != CREATE) {
             pthread_mutex_unlock(&mutex);
-            sleep(1);
             continue;
         }
 
-        int exists = 0;
+        bool exists = false;
 
         for (int i = 0; i < file_count; i++) {
             if (strcmp(files[i].name, current_filename) == 0) {
-                exists = 1;
+                exists = true;
                 break;
             }
         }
 
         if (exists) {
             printf("File '%s' already exists.\n", current_filename);
-        } else if (file_count >= MAX_FILES) {
+        } 
+        else if (file_count >= MAX_FILES) {
             printf("File system full. Cannot create '%s'.\n", current_filename);
-        } else {
+        } 
+        else {
             strncpy(files[file_count].name, current_filename, MAX_FILENAME - 1);
             files[file_count].name[MAX_FILENAME - 1] = '\0';
 
             files[file_count].content[0] = '\0';
             files[file_count].size = 0;
 
-            printf("File '%s' created successfully at index %d.\n",
-                   files[file_count].name,
-                   file_count);
+            printf(
+                "File '%s' created successfully at index %d.\n",
+                files[file_count].name,
+                file_count
+            );
 
             file_count++;
         }
 
         current_command = NONE;
-        current_filename[0] = '\0';
-        current_file_index = -1;
 
         pthread_mutex_unlock(&mutex);
     }
