ea644a21434a413dae5736059486d040a6c5bd63
Johnny Tsai <cljohnnytsai@gmail.com>
2026-05-12 12:20:20 -0700

    Optimize searchThread by removing sleep and logs
    Removed unnecessary sleep and logging for file search.

--- fixedFiles/searchThread.c
+++ fixedFiles/searchThread.c
@@ -11,12 +11,10 @@ void *search_thread(void *arg) {
 
         if (current_command != SEARCH) {
             pthread_mutex_unlock(&mutex);
-            sleep(1);
             continue;
         }
 
         int found = -1;
-
         for (int i = 0; i < file_count; i++) {
             if (strcmp(files[i].name, current_filename) == 0) {
                 found = i;
@@ -25,21 +23,8 @@ void *search_thread(void *arg) {
         }
 
         current_file_index = found;
-
-        if (found == -1) {
-            printf("File '%s' not found.\n", current_filename);
-        } else {
-            printf("File '%s' found at index %d.\n", current_filename, found);
-        }
-
-        if (search_requested_by_open) {
-            current_command = OPEN;
-            pthread_cond_signal(&cond);
-        } else {
-            current_command = NONE;
-            current_filename[0] = '\0';
-            current_file_index = -1;
-        }
+        search_done = true;
+        pthread_cond_signal(&cond);
 
         pthread_mutex_unlock(&mutex);
     }
