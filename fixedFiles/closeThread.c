d71c34ab5c37034e5beaaed863b8059c1c2ba301
Johnny Tsai <cljohnnytsai@gmail.com>
2026-05-12 12:30:31 -0700

    Refactor file closing logic in closeThread.c

--- fixedFiles/closeThread.c
+++ fixedFiles/closeThread.c
@@ -11,30 +11,22 @@ void *close_thread(void *arg) {
 
         if (current_command != CLOSE) {
             pthread_mutex_unlock(&mutex);
-            sleep(1);
             continue;
         }
 
-        if (current_file_index < 0 || current_file_index >= file_count) {
-            printf("No valid file is currently open.\n");
-        } else {
-            strncpy(files[current_file_index].content, global_buffer, MAX_CONTENT - 1);
-            files[current_file_index].content[MAX_CONTENT - 1] = '\0';
+        strncpy(files[current_file_index].content, global_buffer, MAX_CONTENT - 1);
+        files[current_file_index].content[MAX_CONTENT - 1] = '\0';
 
-            files[current_file_index].size =
-                (int)strlen(files[current_file_index].content);
+        files[current_file_index].size = (int)strlen(files[current_file_index].content);
 
-            printf("File '%s' saved and closed. Size: %d/%d characters used.\n",
-                   files[current_file_index].name,
-                   files[current_file_index].size,
-                   MAX_CONTENT - 1);
-        }
+        printf(
+            "File '%s' saved and closed. %d/%d characters used.\n",
+            files[current_file_index].name,
+            files[current_file_index].size,
+            MAX_CONTENT - 1
+        );
 
-        global_buffer[0] = '\0';
         current_command = NONE;
-        current_filename[0] = '\0';
-        current_file_index = -1;
-
         pthread_mutex_unlock(&mutex);
     }
 
